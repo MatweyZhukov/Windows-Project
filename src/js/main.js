2de14c5cb3f2e9c0b8413383965fac6d5b27c2c4
Zhukov Matwey <zhukov.matwei@gmail.com>
2023-03-28 09:14:20 +0300

    windows

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+import "./slider";
+import modals from "./modules/modals";
+import tabs from "./modules/tabs";
+import forms from "./modules/forms";
+import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
+import images from "./modules/images";
+
+window.addEventListener('DOMContentLoaded', () => {
+	const modalState = {},
+		deadline = '2023-04-01';
+
+	modals();
+	tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+	tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+	tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
+	forms('form', 'input', 'input[name="user_phone"]', modalState);
+	changeModalState(modalState);
+	timer('.container1', deadline);
+	images();
+});
+
+alert(5)
