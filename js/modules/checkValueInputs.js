fa60b0e0b652c616025b1eb591b82215d4710080
Zhukov Matwey <106314713+MatweyZhukov@users.noreply.github.com>
2023-06-10 10:43:04 +0300

    Add files via upload

---
+++ js/modules/checkValueInputs.js
@@ -0,0 +1,11 @@
+const checkValueInputs = (trigger) => {
+  const numInputs = document.querySelectorAll(trigger);
+
+  numInputs.forEach((item) => {
+    item.addEventListener("input", () => {
+      item.value = item.value.replace(/\D/, "");
+    });
+  });
+};
+
+export default checkValueInputs;
